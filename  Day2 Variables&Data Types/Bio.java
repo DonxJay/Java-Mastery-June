570698aa5922eeada166f5b91431a8a62c4615c9
Njabulo Phiri <136030182+DonxJay@users.noreply.github.com>
2025-06-05 07:49:23 +0200

    Day2

---
+++  Day2 Variables&Data Types/Bio.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Bio {
+    public static void main(String[] args) {
+      
+      String name = "Njabulo";
+      int age = 27;
+      String city = "Johannesburg";
+      
+      System.out.println("Hi, my name is " + name + "." + "I'm " + age + " years old and I live in " + city + "." );
+      
+    }
+}
