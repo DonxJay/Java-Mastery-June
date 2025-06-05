f099a896cbea2b4233c4e3d93bb6b78cae43955a
Njabulo Phiri <136030182+DonxJay@users.noreply.github.com>
2025-06-05 06:44:23 +0200

    Update Triangle.java

--- JavaTutorials/Day1/Triangle.java
+++ JavaTutorials/Day1/Triangle.java
@@ -1,1 +1,21 @@
-on
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int rows = 3; //number of rows of the triangle
+      
+      for (int i = 1; i <= rows; ++i){
+        for( int j = i; j < rows; ++j){       
+          System.out.print(" ");            //adds spaces before the stars
+        }
+        
+        for (int k = 1; k <= (2 * i -1); ++k){
+          System.out.print("*");                   //prints the stars
+        }
+        
+      System.out.println(" ");
+      }
+      
+  }
+}
